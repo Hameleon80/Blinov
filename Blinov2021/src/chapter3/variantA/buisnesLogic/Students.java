f2b0851b39be11a4feaaf5c96d3dca1177073f36
АхтырскийЮра <@MyBalu>
2021-05-18 16:07:24 +0300

    Continue work with Util class

--- Blinov2021/src/chapter3/variantA/buisnesLogic/Students.java
+++ Blinov2021/src/chapter3/variantA/buisnesLogic/Students.java
@@ -5,8 +5,10 @@ import chapter3.variantA.entity.Student;
 public class Students {
 
 	public static void main(String[] args) {
-		Util.readArrayFromFile("students.txt");
-
+		Student[] students = Util.readStudentArrayFromFile("students.txt");
+		for(int i=0; i<students.length; i++) {
+			System.out.println(students[i]);
+		}
 	}
 
 }
--- Blinov2021/src/chapter3/variantA/buisnesLogic/Util.java
+++ Blinov2021/src/chapter3/variantA/buisnesLogic/Util.java
@@ -3,21 +3,42 @@ package chapter3.variantA.buisnesLogic;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Date;
 import java.util.StringTokenizer;
 
-import chapter3.variantA.entity.Entity;
+import chapter3.variantA.entity.Course;
+import chapter3.variantA.entity.Faculty;
+import chapter3.variantA.entity.Groupe2021;
+import chapter3.variantA.entity.Student;
 
 public class Util {
-	public static Entity[] readArrayFromFile(String file) {
-		Entity[] array=new Entity[1];
+	/**
+	 * Read information about students from text file. Add them in array.
+	 * 
+	 * @param file - file with students
+	 * @return - student's array
+	 */
+	public static Student[] readStudentArrayFromFile(String file) {
+		Student[] array=new Student[5];
 		BufferedReader br=null;
 		try {
 			br=new BufferedReader(new FileReader(file));
 			String line=null;
+			int lineNumber=0;
 			while((line=br.readLine())!=null) {
+				lineNumber++;
 				StringTokenizer tokenStr = new StringTokenizer(line, "|");
 				while(tokenStr.hasMoreTokens()) {
-					
+					array[lineNumber].setId(Integer.parseInt(tokenStr.nextToken()));
+					array[lineNumber].setLastName(tokenStr.nextToken());
+					array[lineNumber].setFirstName(tokenStr.nextToken());
+					array[lineNumber].setMiddleName(tokenStr.nextToken());
+					array[lineNumber].setAdres(tokenStr.nextToken());
+					array[lineNumber].setPhoneNumber(tokenStr.nextToken());
+					array[lineNumber].setBirthday(new Date(Long.parseLong(tokenStr.nextToken())));
+					array[lineNumber].setFaculty(Faculty.valueOf(tokenStr.nextToken().toUpperCase()));
+					array[lineNumber].setCourse(Course.valueOf(tokenStr.nextToken().toUpperCase()));
+					array[lineNumber].setGroupe(Groupe2021.valueOf(tokenStr.nextToken().toUpperCase()));
 				}
 			}
 			br.close();
