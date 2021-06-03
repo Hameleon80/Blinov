ef989aa6e55f2cfa56c674104420839b7dbb07a5
АхтырскийЮра <@192.168.0.58>
2021-06-03 15:16:19 +0300

    varB2

--- Blinov2021/src/chapter3/variantB/entity/Fraction.java
+++ Blinov2021/src/chapter3/variantB/entity/Fraction.java
@@ -39,7 +39,7 @@ public class Fraction extends Parent{
 
 	@Override
 	public Parent addition(Parent el) throws MyException{
-		if (el==null) {
+		if (el==null || !(el instanceof Fraction)) {
 			throw new MyException(ErrorMessage.WRONG_PARAMETER);
 		}
 		Fraction res=new Fraction();
@@ -48,8 +48,9 @@ public class Fraction extends Parent{
 			res.setM(this.getM()+temp.getM());
 			res.setN(this.getN());
 		}else {
-			//найти наименьший общий делитель
-			
+			int d=leastCommonDivisor(this.getN(), temp.getN());
+			res.setM(this.getM()/d+temp.getM()/d);
+			res.setN(d);
 		}
 		return res;
 	}
@@ -70,5 +71,61 @@ public class Fraction extends Parent{
 	public Parent division(Parent el) throws MyException{
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	//helping methods
+	
+	/**
+	 * Finds least common division for two fraction.
+	 * 
+	 * @param first - first fraction's denominator
+	 * @param second - second fraction's denominator
+	 * @return - least common division for two given denominators
+	 * @throws MyException - throws if parameters less than 0
+	 */
+	private int leastCommonDivisor (int first, int second) throws MyException {
+		if (first<=0 || second<=0) {
+			throw new MyException(ErrorMessage.WRONG_PARAMETER);
+		}
+		int res=0;
+		if(first%second==0) {
+			res=first;
+		}else if(second%first==0) {
+			res=second;
+		}else if(isSimpleNumber(first) && isSimpleNumber(second)){
+			res=first*second;
+		}else {
+			int limit=first<second ? first : second;
+			for(int i=2; i<=limit; i++) {
+				if(first%i==0 && second%i==0) {
+					res=i*(first/i)*(second/i);
+					break;
+				}
+			}
+		}
+		return res;
+	}
+	
+	/**
+	 * Checks given number is simple or not.
+	 * 
+	 * @param number - number for check
+	 * @return - true if number simple, false if not
+	 * @throws MyException - throws if parameter less than 0
+	 */
+	private boolean isSimpleNumber(int number) throws MyException {
+		if(number<=0) {
+			throw new MyException(ErrorMessage.WRONG_PARAMETER);
+		}
+		boolean result=true;
+		if(number==1) {
+			return result;
+		}
+		for(int i=2; i<number; i++) {
+			if(number%i==0) {
+				result=false;
+			}
+		}
+		return result;
 	}
 }
--- Blinov2021/src/chapter3/variantB/entity/Parent.java
+++ Blinov2021/src/chapter3/variantB/entity/Parent.java
@@ -3,6 +3,13 @@ package chapter3.variantB.entity;
 import chapter3.variantA.exceptions.MyException;
 
 public abstract class Parent {
+	/**
+	 * Abstract method for addition two objects that extends from Parent class.
+	 * 
+	 * @param el - object that was added to this object
+	 * @return - object that contains addition of two objects
+	 * @throws MyException - exception described in my application
+	 */
 	public abstract Parent addition(final Parent el) throws MyException;
 	public abstract Parent subtraction(final Parent el) throws MyException;
 	public abstract Parent multiplaction(final Parent el) throws MyException;
